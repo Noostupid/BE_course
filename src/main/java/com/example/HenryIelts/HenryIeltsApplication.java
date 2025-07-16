652746ad10471897c6ac4c6085642aad89a3345a
LAPTOP-L6LRSLRD\TUAN MINH <minhltv7@gmail.com>
2025-07-16 20:17:31 +0700

    create project

---
+++ src/main/java/com/example/HenryIelts/HenryIeltsApplication.java
@@ -0,0 +1,13 @@
+package com.example.HenryIelts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenryIeltsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HenryIeltsApplication.class, args);
+	}
+
+}
